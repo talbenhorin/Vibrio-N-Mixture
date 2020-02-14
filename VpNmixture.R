89a3f4f9b59e3a671ba8d2e92264ff10fb54352e
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2020-02-14 16:59:51 -0500

    Add posterior plots

--- VpNmixture.R
+++ VpNmixture.R
@@ -16,7 +16,6 @@ library(MCMCvis)
 library(ggplot2)
 
 dat <- read.csv("seagrantvibrio.csv", fill = FALSE, header = TRUE) 
-m1.dat<-list(c=dat$path,v=dat$mass,samp=dat$samp,gear=dat$gear,tide=dat$t2,mod=dat$mod,hi=dat$hi,time=dat$time)
 
 # N-Mixture model for serial dilution data 
 cat(
@@ -53,41 +52,42 @@ m1.inits <- list(list("U"=numeric(996),"V"=numeric(20),"tau_U"=0.1,"tau_V"=0.1,"
                  list("U"=numeric(996),"V"=numeric(20),"tau_U"=0.01,"tau_V"=0.1,"b0"=0,"b1"=0,"b2"=0,"b3"=0,"b4"=0,"b5"=0),
                  list("U"=numeric(996),"V"=numeric(20),"tau_U"=1,"tau_V"=0.1,"b0"=0,"b1"=0,"b2"=0,"b3"=0,"b4"=0,"b5"=0))
 
-parameters <- c("U","b0","b1","b2","b3","b4","b5")
+parameters <- c("b0","b1","b2","b3","b4","b5")
 
-m1 <- jags(data = m1.dat,
-           inits = m1.inits,
-           parameters.to.save = parameters,
-           model.file = "m1.jag",
-           n.chains = 3,
-           n.iter = 4000,
-           n.burnin = 1000,
-           n.thin = 3)  
+m1.total <- list(c=dat$tlh,v=dat$mass,samp=dat$samp,gear=dat$gear,tide=dat$t2,mod=dat$mod,hi=dat$hi,time=dat$time) #data string, total vibrio
+m1.path <-list(c=dat$path,v=dat$mass,samp=dat$samp,gear=dat$gear,tide=dat$t2,mod=dat$mod,hi=dat$hi,time=dat$time) #data string, pathogenic vibrio
 
+mTotal <- jags(data = m1.total,
+                inits = m1.inits,
+                parameters.to.save = parameters,
+                model.file = "m1.jag",
+                n.chains = 3,
+                n.iter = 4000,
+                n.burnin = 1000,
+                n.thin = 3)  
 
-outvals <- data.frame(
-  b0=m1$BUGSoutput$sims.list$b0,
-  b1=m1$BUGSoutput$sims.list$b1,
-  b2=m1$BUGSoutput$sims.list$b2,
-  b3=m1$BUGSoutput$sims.list$b3,
-  b4=m1$BUGSoutput$sims.list$b4,
-  b5=m1$BUGSoutput$sims.list$b5)
+mPath <- jags(data = m1.path,
+                inits = m1.inits,
+                parameters.to.save = parameters,
+                model.file = "m1.jag",
+                n.chains = 3,
+                n.iter = 4000,
+                n.burnin = 1000,
+                n.thin = 3)
+
+out <- data.frame(
+  Total=mTotal$BUGSoutput$sims.list$b3,
+  Pathogenic=mPath$BUGSoutput$sims.list$b3)
+b3 <- stack(out,select=c('Total','Pathogenic'))
+names(b3) <- c("values","vibrio")
+
+ggplot(b3, aes(x=values, fill=vibrio))+ 
+  geom_density(alpha=0.4)+
+  theme_light()+
+  labs(title=expression(beta[3]),x = "", y = "")
   
-ggplot(outvals, aes(x=b3)) + geom_histogram()
-ggplot(outvals, aes(x=b3)) + 
-  geom_histogram(aes(y=..density..), colour="red", fill="white")+
-  geom_density(alpha=.2, fill="#FF6666") 
 
 
-# Posterior Median and Highest Posterior Density Intervals
-out<-MCMCpstr(m1,
-              params = parameters,
-              func = median,
-              type = 'summary')
-out95<-hdi(list(m1$BUGSoutput$sims.list$b0,
-                m1$BUGSoutput$sims.list$b1,
-                m1$BUGSoutput$sims.list$b2,
-                m1$BUGSoutput$sims.list$b3,
-                m1$BUGSoutput$sims.list$b4,
-                m1$BUGSoutput$sims.list$b5))
+
+  
 
