51e1ae3f03593f4f4d075851b71e8afecec2a86b
Tal Ben-Horin <talbenhorin@Tals-Air.lan1>
2020-02-13 22:06:50 -0500

    update

--- VpNMixture2014.R
+++ VpNMixture2014.R
@@ -5,5 +5,5 @@ library(loo)
 library(HDInterval)
 library(MCMCvis)
 
-dat <- read.csv("seagrantvibrio.csv", fill = FALSE, header = TRUE) 
-
+dat <- read.csv("yr2seagrantvibrio.csv", fill = FALSE, header = TRUE) 
+m1.dat<-list(c=dat$path,v=dat$mass,samp=dat$samp,gear=dat$gear,tide=dat$t2,mod=dat$mod,hi=dat$hi,time=dat$time)
