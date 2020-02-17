04a7559159b61ff42d7eefa41c67e74a719051e4
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2020-02-17 16:51:10 -0500

    more plotting

--- VpNmixture.R
+++ VpNmixture.R
@@ -16,6 +16,7 @@ library(HDInterval)
 library(MCMCvis)
 library(ggplot2)
 library(patchwork)
+library(scales)
 
 dat <- read.csv("seagrantvibrio.csv", fill = FALSE, header = TRUE) 
 
@@ -40,6 +41,7 @@ cat(
     tau_U ~ dgamma(0.1,0.1)
     tau_V ~ dgamma(0.1,0.1)
     b0 ~ dnorm(0,0.1)
+    LOR0 <- exp(b0)
     b1 ~ dnorm(0,0.1)
     LOR1 <- exp(b1)
     b2 ~ dnorm(0,0.1)
@@ -59,7 +61,7 @@ m1.inits <- list(list("U"=numeric(996),"V"=numeric(20),"tau_U"=0.1,"tau_V"=0.1,"
                  list("U"=numeric(996),"V"=numeric(20),"tau_U"=0.01,"tau_V"=0.1,"b0"=0,"b1"=0,"b2"=0,"b3"=0,"b4"=0,"b5"=0),
                  list("U"=numeric(996),"V"=numeric(20),"tau_U"=1,"tau_V"=0.1,"b0"=0,"b1"=0,"b2"=0,"b3"=0,"b4"=0,"b5"=0))
 
-parameters <- c("b0","LOR1","LOR2","LOR3","LOR4","LOR5")
+parameters <- c("LOR0","LOR1","LOR2","LOR3","LOR4","LOR5")
 
 m1.total <- list(c=dat$tlh,v=dat$mass,samp=dat$samp,gear=dat$gear,tide=dat$t2,mod=dat$mod,hi=dat$hi,time=dat$time) #data string, total vibrio
 m1.path <-list(c=dat$path,v=dat$mass,samp=dat$samp,gear=dat$gear,tide=dat$t2,mod=dat$mod,hi=dat$hi,time=dat$time) #data string, pathogenic vibrio
@@ -69,7 +71,7 @@ mTotal <- jags(data = m1.total,
                 parameters.to.save = parameters,
                 model.file = "m1.jag",
                 n.chains = 3,
-                n.iter = 10000,
+                n.iter = 4000,
                 n.burnin = 1000,
                 n.thin = 3)  
 
@@ -78,15 +80,27 @@ mPath <- jags(data = m1.path,
                 parameters.to.save = parameters,
                 model.file = "m1.jag",
                 n.chains = 3,
-                n.iter = 10000,
+                n.iter = 4000,
                 n.burnin = 1000,
                 n.thin = 3)
 
 out0 <- data.frame(
-  Total=mTotal$BUGSoutput$sims.list$b0,
-  Pathogenic=mPath$BUGSoutput$sims.list$b0)
+  Total=mTotal$BUGSoutput$sims.list$LOR0,
+  Pathogenic=mPath$BUGSoutput$sims.list$LOR0)
 b0 <- stack(out0,select=c('Total','Pathogenic'))
 names(b0) <- c("values","vibrio")
+
+out1 <- data.frame(
+  Total=mTotal$BUGSoutput$sims.list$LOR1,
+  Pathogenic=mPath$BUGSoutput$sims.list$LOR1)
+b1 <- stack(out1,select=c('Total','Pathogenic'))
+names(b1) <- c("values","vibrio")
+
+out2 <- data.frame(
+  Total=mTotal$BUGSoutput$sims.list$LOR2,
+  Pathogenic=mPath$BUGSoutput$sims.list$LOR2)
+b2 <- stack(out2,select=c('Total','Pathogenic'))
+names(b2) <- c("values","vibrio")
 
 out3 <- data.frame(
   Total=mTotal$BUGSoutput$sims.list$LOR3,
@@ -94,13 +108,54 @@ out3 <- data.frame(
 b3 <- stack(out3,select=c('Total','Pathogenic'))
 names(b3) <- c("values","vibrio")
 
+out4 <- data.frame(
+  Total=mTotal$BUGSoutput$sims.list$LOR4,
+  Pathogenic=mPath$BUGSoutput$sims.list$LOR4)
+b4 <- stack(out4,select=c('Total','Pathogenic'))
+names(b4) <- c("values","vibrio")
 
-p1 <- ggplot(b0, aes(x=values, fill=vibrio))+ 
+out5 <- data.frame(
+  Total=mTotal$BUGSoutput$sims.list$LOR5,
+  Pathogenic=mPath$BUGSoutput$sims.list$LOR5)
+b5 <- stack(out5,select=c('Total','Pathogenic'))
+names(b5) <- c("values","vibrio")
+
+
+p0 <- ggplot(b0, aes(x=values, fill=vibrio))+ 
   geom_density(alpha=0.4)+
   theme_light()+
-  labs(title=expression(beta[0]),x = "", y = "")
-p2 <- ggplot(b3, aes(x=values, fill=vibrio))+ 
+  scale_x_log10(breaks = c(1,10,100,1000),
+                labels = c("1" = "1","10" = "10","100"="100","1000"="1000"))+
+  labs(title=expression(beta[0]),x = "", y = "")+
+  theme(plot.title = element_text(hjust = 0.5))
+p1 <- ggplot(b1, aes(x=values, fill=vibrio))+ 
   geom_density(alpha=0.4)+
   theme_light()+
-  labs(title=expression(beta[3]),x = "", y = "")
-(p1 / p2)
+  xlim(0,3)+
+  labs(title=expression(beta[1]),x = "", y = "")+
+  theme(plot.title = element_text(hjust = 0.5))
+p2 <- ggplot(b2, aes(x=values, fill=vibrio))+ 
+  geom_density(alpha=0.4)+
+  theme_light()+
+  xlim(0,3)+
+  labs(title=expression(beta[2]),x = "", y = "")+
+  theme(plot.title = element_text(hjust = 0.5))
+p3 <- ggplot(b3, aes(x=values, fill=vibrio))+ 
+  geom_density(alpha=0.4)+
+  theme_light()+
+  xlim(0,3)+
+  labs(title=expression(beta[3]),x = "", y = "")+
+  theme(plot.title = element_text(hjust = 0.5))
+p4 <- ggplot(b4, aes(x=values, fill=vibrio))+ 
+  geom_density(alpha=0.4)+
+  theme_light()+
+  xlim(0,3)+
+  labs(title=expression(beta[4]),x = "", y = "")+
+  theme(plot.title = element_text(hjust = 0.5))
+p5 <- ggplot(b5, aes(x=values, fill=vibrio))+ 
+  geom_density(alpha=0.4)+
+  theme_light()+
+  xlim(0,3)+
+  labs(title=expression(beta[5]),x = "", y = "")+
+  theme(plot.title = element_text(hjust = 0.5))
+(p0 / p1 / p2 / p3 / p4 / p5)
