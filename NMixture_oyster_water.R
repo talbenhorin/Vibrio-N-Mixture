6ac347cae636038c935f6e461f634997f84d8494
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2021-01-25 15:51:27 -0500

    Update

--- NMixture_oyster_water.R
+++ NMixture_oyster_water.R
@@ -2,14 +2,13 @@ rm(list=ls(all=TRUE))
 
 # Download JAGS at https://sourceforge.net/projects/mcmc-jags/files/JAGS/4.x/Windows/
 # Load these packages the first time your run R
-install.packages("RTools")
-install.packages("rjags")
-install.packages("coda")
-install.packages("R2jags")
-install.packages("hdi")
-install.packages("MCMCvis")
-install.packages("loo")
-install.packages("HDInterval")
+#install.packages("RTools")
+#install.packages("rjags")
+#install.packages("coda")
+#install.packages("R2jags")
+#install.packages("hdi")
+#install.packages("MCMCvis")
+#install.packages("patchwork")
 
 library(R2jags)
 library(loo)
@@ -18,97 +17,64 @@ library(MCMCvis)
 library(ggplot2)
 library(patchwork)
 library(scales)
+library(bayesplot)
 
-dat <- read.csv("va2014water.csv", fill = FALSE, header = TRUE) 
+dat <- read.csv("vaoysterwater.csv", fill = FALSE, header = TRUE) 
+vibrio <- list(c=dat$vvha,v=dat$mass,samp=dat$fid,site=dat$site,temp=dat$temp,water=dat$water,watervib=dat$water.vvha) #data string, total vibrio
 
-# N-Mixture model for serial dilution data 
 cat(
   "model{
-<<<<<<< HEAD
     for (i in 1:288) {
-=======
-    for (i in 1:1362) {
->>>>>>> bb652fa6ef87a040c32aeaf636efacea854e4049
       # Observation model across serial dilutions
       c[i] ~ dbin(p[i],3)
       p[i] <- 1-exp(-MPN[i]*v[i])
       
       # Biological model for microbial abundance
       MPN[i] ~ dpois(lambda[i])
-<<<<<<< HEAD
-      log(lambda[i]) <- b0 + b1*W[water[i]] + U[site[i]] + V[samp[i]]
-      post.prob[i] <- pbin(c[i],p[i],3) + ppois(MPN[i],lambda[i])
-=======
-      log(lambda[i]) <- b0 + b1*chlo[i] + U[site[i]] + V[samp[i]]
-      log.like[i] <- log(pbin(c[i],p[i],3))*log(ppois(MPN[i],lambda[i]))
->>>>>>> edd59bb33139fc9a512b0c84dda658927cb111ea
+      log(lambda[i]) <- b0 + U[site[i]] + V[samp[i]]
+      
+      # likelihood function for loo
+      like[i] <- log(pbin(c[i],p[i],3))
+      
     }
     for (s in 1:3) {
       U[s] ~ dnorm(0,tau_U)
     }
-<<<<<<< HEAD
     for (t in 1:48) {
       V[t] ~ dnorm(0,tau_V)
     }
-=======
-    for (t in 1:222) {
-      V[t] ~ dnorm(0,tau_V)
-    }
-    for (w in 1:56) {
-      W[w] <- mu_w[w]
-    }
->>>>>>> bb652fa6ef87a040c32aeaf636efacea854e4049
     tau_U ~ dgamma(0.1,0.1)
     tau_V ~ dgamma(0.1,0.1)
     b0 ~ dnorm(0,0.1)
-    b1 ~ dnorm(0,0.1)
-    r ~ dunif(0,50)
+
+    #sum log likihoods for loo
+    log.like <- sum(like)
+    
   }",
-  file="water.jag"
+  file="model1.jag"
 )
 
-# Initial params BOTH YEARS
-<<<<<<< HEAD
-inits <- list(list("U"=numeric(3),"V"=numeric(48),"tau_U"=0.1,"tau_V"=0.1,"b0"=0,"b1"=0),
-                   list("U"=numeric(3),"V"=numeric(48),"tau_U"=0.01,"tau_V"=0.1,"b0"=0,"b1"=0),
-                   list("U"=numeric(3),"V"=numeric(48),"tau_U"=1,"tau_V"=0.1,"b0"=0,"b1"=0))
+base.inits <- list(list("U"=numeric(3),"V"=numeric(48),"tau_U"=0.1,"tau_V"=0.1,"b0"=1),
+                   list("U"=numeric(3),"V"=numeric(48),"tau_U"=0.01,"tau_V"=0.1,"b0"=1),
+                   list("U"=numeric(3),"V"=numeric(48),"tau_U"=1,"tau_V"=0.1,"b0"=1))
 
-pfull <- c("b0","b1","log.like")
+params <- c("log.like")
 
-in.data <- list(c=dat$Vv.vvha,v=dat$Sample.Volume,samp=dat$FID,site=dat$Site.Num,chlo=dat$chlo.t) #data string
-=======
-inits <- list(list("U"=numeric(4),"V"=numeric(222),"tau_U"=0.1,"tau_V"=0.1,"b0"=0,"b1"=0),
-                   list("U"=numeric(4),"V"=numeric(222),"tau_U"=0.01,"tau_V"=0.1,"b0"=0,"b1"=0),
-                   list("U"=numeric(4),"V"=numeric(222),"tau_U"=1,"tau_V"=0.1,"b0"=0,"b1"=0))
+m <- jags(data = vibrio,
+          inits = base.inits,
+          parameters.to.save = params,
+          model.file = "model1.jag",
+          n.chains = 3,
+          n.iter = 10000,
+          n.burnin = 1000,
+          n.thin = 3)
 
-pfull <- c("post.prob")
+m.parmlist <- m$BUGSoutput$sims.list
+m.loglike <- m.parmlist$log.like
 
-in.data <- list(c=dat$pilf,v=dat$mass,samp=dat$fid,site=dat$site,water=dat$water,mu_w=dat$water.vvha) #data string
->>>>>>> bb652fa6ef87a040c32aeaf636efacea854e4049
-
-m.base <- jags(data = in.data,
-               inits = inits,
-               parameters.to.save = pfull,
-               model.file = "water.jag",
-               n.chains = 3,
-               n.iter = 4000,
-               n.burnin = 1000,
-               n.thin = 3)
-
-
-<<<<<<< HEAD
-
-
-=======
-#out <-MCMCpstr(m.base,
-#              params = pfull,
-#              func = median,
-#              type = 'summary')
-b0.95 <- hdi(m.base$BUGSoutput$sims.list$b0)
-b1.95 <- hdi(m.base$BUGSoutput$sims.list$b1)
-<<<<<<< HEAD
-
-=======
->>>>>>> bb652fa6ef87a040c32aeaf636efacea854e4049
->>>>>>> edd59bb33139fc9a512b0c84dda658927cb111ea
-
+m.loo <- loo(m.loglike, r_eff = NA)
+m.loo
+m.mcmc <- as.mcmc(m)
+m.gel <- gelman.diag(m.mcmc, confidence = 0.95, transform=FALSE, autoburnin=TRUE,
+                     multivariate=TRUE)
+m.gel
